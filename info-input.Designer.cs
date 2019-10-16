22ec52150fa3f96ba78df6d1b260d252e718b8cf
Nguyen Dinh Huong <DinhHuong.Ng@samsung.com>
2019-10-16 17:10:07 +0700

    Add info-input form
    Signed-off-by: Nguyen Dinh Huong <DinhHuong.Ng@samsung.com>

---
+++ info-input.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace DxPropPages
+{
+    partial class info_input
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grB_info = new System.Windows.Forms.GroupBox();
+            this.lb_ten_dieu_tra_vien = new System.Windows.Forms.Label();
+            this.txtB_ten_dieu_tra_vien = new System.Windows.Forms.TextBox();
+            this.txtB_don_vi = new System.Windows.Forms.TextBox();
+            this.lb_don_vi = new System.Windows.Forms.Label();
+            this.txtB_ten_doi_tuong = new System.Windows.Forms.TextBox();
+            this.lb_ten_doi_tuong = new System.Windows.Forms.Label();
+            this.txtB_dia_diem = new System.Windows.Forms.TextBox();
+            this.lb_dia_diem = new System.Windows.Forms.Label();
+            this.txtB_ten_vu_an = new System.Windows.Forms.TextBox();
+            this.lb_ten_vu_an = new System.Windows.Forms.Label();
+            this.lb_ngay_gio = new System.Windows.Forms.Label();
+            this.lb_ngay_gio_system = new System.Windows.Forms.Label();
+            this.btn_xong = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grB_info.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grB_info
+            // 
+            this.grB_info.Controls.Add(this.btn_xong);
+            this.grB_info.Controls.Add(this.lb_ngay_gio_system);
+            this.grB_info.Controls.Add(this.lb_ngay_gio);
+            this.grB_info.Controls.Add(this.txtB_ten_vu_an);
+            this.grB_info.Controls.Add(this.lb_ten_vu_an);
+            this.grB_info.Controls.Add(this.txtB_dia_diem);
+            this.grB_info.Controls.Add(this.lb_dia_diem);
+            this.grB_info.Controls.Add(this.txtB_ten_doi_tuong);
+            this.grB_info.Controls.Add(this.lb_ten_doi_tuong);
+            this.grB_info.Controls.Add(this.txtB_don_vi);
+            this.grB_info.Controls.Add(this.lb_don_vi);
+            this.grB_info.Controls.Add(this.txtB_ten_dieu_tra_vien);
+            this.grB_info.Controls.Add(this.lb_ten_dieu_tra_vien);
+            this.grB_info.Location = new System.Drawing.Point(162, 30);
+            this.grB_info.Name = "grB_info";
+            this.grB_info.Size = new System.Drawing.Size(655, 373);
+            this.grB_info.TabIndex = 0;
+            this.grB_info.TabStop = false;
+            this.grB_info.Text = "Kê khai thông tin";
+            // 
+            // lb_ten_dieu_tra_vien
+            // 
+            this.lb_ten_dieu_tra_vien.AutoSize = true;
+            this.lb_ten_dieu_tra_vien.Location = new System.Drawing.Point(54, 56);
+            this.lb_ten_dieu_tra_vien.Name = "lb_ten_dieu_tra_vien";
+            this.lb_ten_dieu_tra_vien.Size = new System.Drawing.Size(91, 13);
+            this.lb_ten_dieu_tra_vien.TabIndex = 0;
+            this.lb_ten_dieu_tra_vien.Text = "Tên điều tra viên:";
+            // 
+            // txtB_ten_dieu_tra_vien
+            // 
+            this.txtB_ten_dieu_tra_vien.Location = new System.Drawing.Point(162, 53);
+            this.txtB_ten_dieu_tra_vien.Name = "txtB_ten_dieu_tra_vien";
+            this.txtB_ten_dieu_tra_vien.Size = new System.Drawing.Size(378, 20);
+            this.txtB_ten_dieu_tra_vien.TabIndex = 1;
+            // 
+            // txtB_don_vi
+            // 
+            this.txtB_don_vi.Location = new System.Drawing.Point(162, 85);
+            this.txtB_don_vi.Name = "txtB_don_vi";
+            this.txtB_don_vi.Size = new System.Drawing.Size(378, 20);
+            this.txtB_don_vi.TabIndex = 3;
+            // 
+            // lb_don_vi
+            // 
+            this.lb_don_vi.AutoSize = true;
+            this.lb_don_vi.Location = new System.Drawing.Point(54, 88);
+            this.lb_don_vi.Name = "lb_don_vi";
+            this.lb_don_vi.Size = new System.Drawing.Size(41, 13);
+            this.lb_don_vi.TabIndex = 2;
+            this.lb_don_vi.Text = "Đơn vị:";
+            // 
+            // txtB_ten_doi_tuong
+            // 
+            this.txtB_ten_doi_tuong.Location = new System.Drawing.Point(162, 120);
+            this.txtB_ten_doi_tuong.Name = "txtB_ten_doi_tuong";
+            this.txtB_ten_doi_tuong.Size = new System.Drawing.Size(378, 20);
+            this.txtB_ten_doi_tuong.TabIndex = 5;
+            // 
+            // lb_ten_doi_tuong
+            // 
+            this.lb_ten_doi_tuong.AutoSize = true;
+            this.lb_ten_doi_tuong.Location = new System.Drawing.Point(54, 123);
+            this.lb_ten_doi_tuong.Name = "lb_ten_doi_tuong";
+            this.lb_ten_doi_tuong.Size = new System.Drawing.Size(77, 13);
+            this.lb_ten_doi_tuong.TabIndex = 4;
+            this.lb_ten_doi_tuong.Text = "Tên đối tượng:";
+            // 
+            // txtB_dia_diem
+            // 
+            this.txtB_dia_diem.Location = new System.Drawing.Point(162, 158);
+            this.txtB_dia_diem.Name = "txtB_dia_diem";
+            this.txtB_dia_diem.Size = new System.Drawing.Size(378, 20);
+            this.txtB_dia_diem.TabIndex = 7;
+            // 
+            // lb_dia_diem
+            // 
+            this.lb_dia_diem.AutoSize = true;
+            this.lb_dia_diem.Location = new System.Drawing.Point(54, 161);
+            this.lb_dia_diem.Name = "lb_dia_diem";
+            this.lb_dia_diem.Size = new System.Drawing.Size(86, 13);
+            this.lb_dia_diem.TabIndex = 6;
+            this.lb_dia_diem.Text = "Địa điểm xét hỏi:";
+            // 
+            // txtB_ten_vu_an
+            // 
+            this.txtB_ten_vu_an.Location = new System.Drawing.Point(162, 200);
+            this.txtB_ten_vu_an.Name = "txtB_ten_vu_an";
+            this.txtB_ten_vu_an.Size = new System.Drawing.Size(378, 20);
+            this.txtB_ten_vu_an.TabIndex = 9;
+            // 
+            // lb_ten_vu_an
+            // 
+            this.lb_ten_vu_an.AutoSize = true;
+            this.lb_ten_vu_an.Location = new System.Drawing.Point(54, 203);
+            this.lb_ten_vu_an.Name = "lb_ten_vu_an";
+            this.lb_ten_vu_an.Size = new System.Drawing.Size(99, 13);
+            this.lb_ten_vu_an.TabIndex = 8;
+            this.lb_ten_vu_an.Text = "Tên vụ án/vụ việc:";
+            // 
+            // lb_ngay_gio
+            // 
+            this.lb_ngay_gio.AutoSize = true;
+            this.lb_ngay_gio.Location = new System.Drawing.Point(54, 247);
+            this.lb_ngay_gio.Name = "lb_ngay_gio";
+            this.lb_ngay_gio.Size = new System.Drawing.Size(52, 13);
+            this.lb_ngay_gio.TabIndex = 10;
+            this.lb_ngay_gio.Text = "Ngày giờ:";
+            // 
+            // lb_ngay_gio_system
+            // 
+            this.lb_ngay_gio_system.AutoSize = true;
+            this.lb_ngay_gio_system.Location = new System.Drawing.Point(159, 247);
+            this.lb_ngay_gio_system.Name = "lb_ngay_gio_system";
+            this.lb_ngay_gio_system.Size = new System.Drawing.Size(91, 13);
+            this.lb_ngay_gio_system.TabIndex = 11;
+            this.lb_ngay_gio_system.Text = "15:20 10-10-2019";
+            // 
+            // btn_xong
+            // 
+            this.btn_xong.Location = new System.Drawing.Point(288, 299);
+            this.btn_xong.Name = "btn_xong";
+            this.btn_xong.Size = new System.Drawing.Size(99, 46);
+            this.btn_xong.TabIndex = 12;
+            this.btn_xong.Text = "XONG";
+            this.btn_xong.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DxPropPages.Properties.Resources.pro;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 138);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // info_input
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 418);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.grB_info);
+            this.Name = "info_input";
+            this.Text = "Nhập thông tin";
+            this.grB_info.ResumeLayout(false);
+            this.grB_info.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grB_info;
+        private System.Windows.Forms.TextBox txtB_ten_dieu_tra_vien;
+        private System.Windows.Forms.Label lb_ten_dieu_tra_vien;
+        private System.Windows.Forms.TextBox txtB_don_vi;
+        private System.Windows.Forms.Label lb_don_vi;
+        private System.Windows.Forms.TextBox txtB_ten_doi_tuong;
+        private System.Windows.Forms.Label lb_ten_doi_tuong;
+        private System.Windows.Forms.Button btn_xong;
+        private System.Windows.Forms.Label lb_ngay_gio_system;
+        private System.Windows.Forms.Label lb_ngay_gio;
+        private System.Windows.Forms.TextBox txtB_ten_vu_an;
+        private System.Windows.Forms.Label lb_ten_vu_an;
+        private System.Windows.Forms.TextBox txtB_dia_diem;
+        private System.Windows.Forms.Label lb_dia_diem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
